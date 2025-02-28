ce8cbd8973c9915f4023672fa92cf2e15b426ec1
AMIROLIMI <oamir3867@mail.ru>
2025-02-28 18:01:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header("Amir Olimi")
+st.header("Amirjon Olimi")
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
 
