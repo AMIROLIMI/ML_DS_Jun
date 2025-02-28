559c14b9fe612baf449b3bf564f5e06528157be3
AMIROLIMI <oamir3867@mail.ru>
2025-02-28 22:34:28 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ def classification_models(data):
     y = data["A16"]
     
     # Разделение на обучающую и тестовую выборки
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
     
     # Стандартизация признаков
     scaler = StandardScaler()
